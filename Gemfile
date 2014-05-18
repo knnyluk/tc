81b0744da8cbbdc913ec0b42a78010a5ff27214a
Ken Luk <knnyluk@gmail.com>
2014-05-18 18:25:18 -0500

    added custom named routes and route tests

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
+gem 'bootstrap-sass'
+gem 'sprockets'
 # Use postgresql as the database for Active Record
 gem 'pg'
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  # get 'static_pages/contact'
   get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  root 'static_pages#home'
   resources :microposts
 
   resources :users
